351eb7a49481968625c41c5154e637f0bad1ca04
Parsa Poorsistani <poorsistani13@gmail.com>
2022-08-20 22:24:13 +0430

    app.js initialize

--- jwt/app.js
+++ jwt/app.js
@@ -1,2 +1,21 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+app.use(notFoundMiddleware);
+app.use(express.json());
+app.use(errorHandlerMiddleware);
+
+
+const start = async() => {
+    try {
+        app.listen(port, () => console.log(`server running on port: ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
--- jwt/controller/apis.js
+++ jwt/controller/apis.js
@@ -1,10 +1,15 @@
 const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
 
 const login = (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
         throw new CustomAPIError('please provide email and password', 400);
     }
+    const id = new Date().getDate();
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+    res.status(201).json({ msg: 'user created', token });
 };
 
 const dashboard = (req, res) => {
